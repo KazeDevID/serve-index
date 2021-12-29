ea57041d80490721ca9496af21619f766b7373c8
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-12-29 19:27:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -99,13 +99,13 @@ function serveIndex(root, options) {
   var view = opts.view || 'tiles';
 
   return function (req, res, next) {
-    if (req.method !== 'GET' && req.method !== 'HEAD') {
+    /* if (req.method !== 'GET' && req.method !== 'HEAD') {
       res.statusCode = 'OPTIONS' === req.method ? 200 : 405;
-      res.setHeader('Allow', 'GET, HEAD, OPTIONS');
+      res.Header('Allow', 'GET, HEAD, OPTIONS');
       res.setHeader('Content-Length', '0');
       res.end();
       return;
-    }
+    }*/
 
     // get dir
     var dir = getRequestedDir(req)
